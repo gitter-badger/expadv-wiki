67a9afe8a474d86acf561152c90dafa21f6c5ce4
Родион Борисов <vintproykt@mail.ru>
2016-01-13 18:35:44 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -1,25 +1,27 @@
 // Required: smartTables.js
 
 document.addEventListener("DOMContentLoaded", function() {
-	var filePath = gitbook.state.filepath;
-	var classFinder;
 	var classInfo;
-
-	classFinder = /^classes\/(.+)\/README\.md$/.exec(filePath);
 
 	var Page = document.querySelector(".page-inner");
 
-	if (classFinder) {
-		var header = Page.querySelector("h1");
-		header.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ header.innerHTML;
-		smartTables.register("info", {
-			"TableTitle": "Class Information",
-			"init": "Initialized in component",
-			"serial": "Can serialize value?",
-			"short": "Short name",
-			"lua-default": "Default value (as seen in Lua)"
+	if (require.specified("gitboook")) {
+		require(["gitbook"], function (gitbook) {
+			var filePath = gitbook.state.filepath;
+			var classFinder = /^classes\/(.+)\/README\.md$/.exec(filePath);
+			if (classFinder) {
+				var header = Page.querySelector("h1");
+				header.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ header.innerHTML;
+				smartTables.register("info", {
+					"TableTitle": "Class Information",
+					"init": "Initialized in component",
+					"serial": "Can serialize value?",
+					"short": "Short name",
+					"lua-default": "Default value (as seen in Lua)"
+				});
+			}
+
+			smartTables.retrieve(Page);
 		});
 	}
-
-	smartTables.retrieve(Page);
 });
