00241a0a91f85aa73b4f56ed23b32a0d5a92e65c
Родион Борисов <vintproykt@mail.ru>
2016-01-14 00:53:50 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -2,11 +2,13 @@
 // Required: compact_GitHub_API.js
 
 var PageInner;
+var PageHeader;
 
 (function() {
     var state = document.readyState;
     if (state == "interactive" || state == "complete") {
         PageInner = document.querySelector(".page-inner");
+		PageHeader = PageInner.querySelector("h1");
 
 		smartTables.register("component", "Component Overview", {
 			"author": [
@@ -29,11 +31,12 @@ var PageInner;
 			"short": "Short name",
 			"lua-default": "Default value (as seen in Lua)"
 		}, function(table) {
-			var header = PageInner.querySelector("h1");
-			header.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ header.innerHTML;
+			PageHeader.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ PageHeader.innerHTML;
 		});
 
 		smartTables.retrieve(PageInner);
+		
+		CGHAPI.fetchContributors("lua/expadv/components/angle.lua")
     }
     else setTimeout(arguments.callee, 10);
 })();
