db2deee706016666738f856af7ed1de4b47bc9dd
Родион Борисов <vintproykt@mail.ru>
2016-01-14 03:41:54 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -1,5 +1,5 @@
 // Required: smartTables.js
-// Required: compact_GitHub_API.js
+// Required: CGHAPI.js
 
 CGHAPI.repo = "Rusketh/ExpAdv2";
 var PageInner;
@@ -13,13 +13,24 @@ var PageHeader;
 		var Title = PageHeader.innerText.trim();
 
 		smartTables.register("component", "Component Overview", {
-			"author": "Author/maintainer",
+			"author": ["Author/maintainer", function(i, oldName, nC, vC) {
+				var r = nC.parentNode.parentNode.insertRow(i + 1);
+				r.insertCell(0).innerText = "contrib";
+				r.insertCell(1).innerHTML = "<i class=\"fa fa-lemon-o fa-spin\"></i>";
+			}],
+			"contrib": "Contributors",
 			"classes": "Classes initialized",
 			"desc": "Short description"
 		}, function(table) {
 			PageHeader.innerHTML = "<i class=\"fa fa-cube\"></i> Component: "+ Title;
-			CGHAPI.fetchContributors("lua/expadv/components/"+ Title +".lua", function(c) {
-				console.log(c);
+			CGHAPI.fetchContributors("lua/expadv/components/"+ Title +".lua", function(contrib) {
+				console.log(contrib);
+				var r = table.querySelector("[data-name='contrib']");
+				var v = r.cells[1];
+				v.innerHTML = "";
+				for (var c in contrib) {
+					v.innerHTML += "<a href=\""+ contrib[c].html_url +"\" target=\"_blank\" title=\""+ c +"\"><img src=\""+ contrib[c].avatar_url +"\"></a>";
+				}
 			});
 		});
 
