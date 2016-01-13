b4a994fe5af4a48ec26d714431c76f5f18ac0322
Родион Борисов <vintproykt@mail.ru>
2016-01-13 19:52:31 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -1,15 +1,23 @@
 // Required: smartTables.js
 
-var Page = document.querySelector(".page-inner");
+var PageInner;
 
-smartTables.register("class", "Class Information", {
-	"init": "Initialized in component",
-	"serial": "Can serialize value?",
-	"short": "Short name",
-	"lua-default": "Default value (as seen in Lua)"
-}, function(table) {
-	var header = Page.querySelector("h1");
-	header.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ header.innerHTML;
-});
+(function() {
+    var state = document.readyState;
+    if (state == "interative" || state == "complete") {
+        PageInner = document.querySelector(".page-inner");
 
-smartTables.retrieve(Page);
+		smartTables.register("class", "Class Information", {
+			"init": "Initialized in component",
+			"serial": "Can serialize value?",
+			"short": "Short name",
+			"lua-default": "Default value (as seen in Lua)"
+		}, function(table) {
+			var header = PageInner.querySelector("h1");
+			header.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ header.innerHTML;
+		});
+
+		smartTables.retrieve(PageInner);
+    }
+    else setTimeout(arguments.callee, 10);
+})();
