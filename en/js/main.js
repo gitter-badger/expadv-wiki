4bc8dc912bdf538d7e59d864f788468c457d95d4
Родион Борисов <vintproykt@mail.ru>
2016-01-13 10:21:17 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -1,47 +1,26 @@
-function smartTable(tBody, nameRepl) {
-	var data = {};
-	var rows = tBody.rows;
-	for (var i = 0; i < rows.length; i++) {
-		var r = rows[i].cells;
-		var name = r[0].innerText.trim();
-		var val = r[1].innerText.trim();
-		var valLower = val.toLowerCase();
-		if (valLower == "no") data[name] = false;
-		else if (valLower == "yes") data[name] = true;
-		else if (!isNaN(val)) data[name] = +val;
-		else data[name] = val;
-		r[0].innerText = nameRepl[name] || name;
-	}
-	return data;
-}
+// Required: smartTables.js
 
 document.addEventListener("DOMContentLoaded", function() {
 	var filePath = gitbook.state.filepath;
 	var classFinder;
 	var classInfo;
-	var classInfoRepl = {
-		"init": "Initialized in component",
-		"serial": "Can serialize value?",
-		"short": "Short name",
-		"lua-default": "Default value (as seen in Lua)"
-	};
 
 	classFinder = /^classes\/(.+)\/README\.md$/.exec(filePath);
 
+	var Page = document.querySelector(".page-inner");
+
 	if (classFinder) {
-		var header = document.querySelector(".page-inner h1");
+		var header = Page.querySelector("h1");
 		header.innerHTML = "Class: "+ header.innerHTML;
 		
-		var tables = document.querySelectorAll(".page-inner table");
-		for (var i = 0; i < tables.length; i++) {
-			var table = tables[i];
-			if (table.tHead) {
-				var name = table.tHead.rows[0].cells[0].innerHTML;
-				if (name == "info") {
-					classInfo = smartTable(table.querySelector("tbody"), classInfoRepl);
-					console.log(classInfo);
-				}
-			}
-		}
+		smartTables.register("info", {
+			"TableTitle": "Class Information",
+			"init": "Initialized in component",
+			"serial": "Can serialize value?",
+			"short": "Short name",
+			"lua-default": "Default value (as seen in Lua)"
+		});
+		var Data = smartTables.retrieve(Page);
+		console.log(Data);
 	}
 });
