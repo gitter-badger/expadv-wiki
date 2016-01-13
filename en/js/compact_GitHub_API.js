412760e60f127c270b393a27d8f9e60af8757188
Родион Борисов <vintproykt@mail.ru>
2016-01-14 00:42:04 +0900

    Update en/js/compact_GitHub_API.js

--- en/js/compact_GitHub_API.js
+++ en/js/compact_GitHub_API.js
@@ -1,1 +1,15 @@
-var CGHAPI = {};
+var CGHAPI = {
+    fetchContributors: function() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', '//example.org', true);
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4) {
+                if(xmlhttp.status == 200) {
+                    var obj = JSON.parse(xmlhttp.responseText);
+                    var countryList = obj.countrylist;
+                 }
+            }
+    };
+    xmlhttp.send(null);
+    }
+}
