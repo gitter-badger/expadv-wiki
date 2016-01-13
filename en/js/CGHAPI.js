64492c9bfba992c8cfc304253cd11f8e9b99ee48
Родион Борисов <vintproykt@mail.ru>
2016-01-14 02:19:26 +0900

    Update en/js/CGHAPI.js

--- en/js/CGHAPI.js
+++ en/js/CGHAPI.js
@@ -5,23 +5,21 @@ Author: VINTproYKT
 
 var CGHAPI = {
 	contributors: {},
-	fetchContributors: function(path) {
+	fetchContributors: function(path, cb) {
 		var self = this;
+		this.fetchCb = cb;
 		var xmlhttp = new XMLHttpRequest();
 		xmlhttp.open("GET", "https://api.github.com/repos/"+ this.repo +"/commits?path="+ path, true);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
 				if (xmlhttp.status == 200) {
-					self.contributors[path] = [];
+					self.contributors[path] = {};
 					var comm = JSON.parse(xmlhttp.responseText);
 					for (var i = 0; i < comm.length; i++) {
 						var author = comm[i].author;
-						if (self.contributors[path].indexOf(author) == -1) self.contributors[path].push(author);
+						self.contributors[path][author.login] = author;
 					}
-					self.contributors[path].sort(function(a, b) {
-						if (b.login > a.login) return 1;
-						else return -1;
-					});
+					CGHAPI.fetchCb(self.contributors[path]);
 				}
 			}
 		};
