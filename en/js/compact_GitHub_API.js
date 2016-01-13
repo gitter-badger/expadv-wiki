382f4c4941ce4c7473805b54c9796cda57f46b9f
Родион Борисов <vintproykt@mail.ru>
2016-01-14 00:46:13 +0900

    Update en/js/compact_GitHub_API.js

--- en/js/compact_GitHub_API.js
+++ en/js/compact_GitHub_API.js
@@ -1,12 +1,13 @@
 var CGHAPI = {
+	repo: "Rusketh/ExpAdv2",
 	fetchContributors: function() {
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open('GET', '//example.org', true);
+		xmlhttp.open("GET", "https://api.github.com/repos/"+ this.repo +"/commits?path=", true);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
 				if(xmlhttp.status == 200) {
 					var obj = JSON.parse(xmlhttp.responseText);
-					var countryList = obj.countrylist;
+					console.log(obj);
 				}
 			}
 		};
