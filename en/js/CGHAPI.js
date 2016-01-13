54862673bcb8a8546d7bc4b9fc58d2e2590d1095
Родион Борисов <vintproykt@mail.ru>
2016-01-14 01:29:58 +0900

    Update en/js/CGHAPI.js

--- en/js/CGHAPI.js
+++ en/js/CGHAPI.js
@@ -10,9 +10,17 @@ var CGHAPI = {
 		xmlhttp.open("GET", "https://api.github.com/repos/"+ this.repo +"/commits?path="+ path, true);
 		xmlhttp.onreadystatechange = function() {
 			if (xmlhttp.readyState == 4) {
-				if(xmlhttp.status == 200) {
-					var obj = JSON.parse(xmlhttp.responseText);
-					console.log(obj);
+				if (xmlhttp.status == 200) {
+					this.contributors[path] = [];
+					var comm = JSON.parse(xmlhttp.responseText);
+					for (var i = 0; i < comm.length; i++) {
+						var author = comm[i].author;
+						if (this.contributors[path].indexOf(author) == -1) this.contributors[path].push(author);
+					}
+					this.contributors[path].sort(function(a, b) {
+						if (b.login > a.login) return 1;
+						else return -1;
+					});
 				}
 			}
 		};
