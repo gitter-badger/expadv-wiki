8f25b26d975357017ecb8753bf4b6f892bd0a582
Родион Борисов <vintproykt@mail.ru>
2016-01-14 00:56:20 +0900

    Update en/js/main.js

--- en/js/main.js
+++ en/js/main.js
@@ -9,7 +9,7 @@ var PageHeader;
     if (state == "interactive" || state == "complete") {
         PageInner = document.querySelector(".page-inner");
 		PageHeader = PageInner.querySelector("h1");
-		var Title = PageHeader.innerText;
+		var Title = PageHeader.innerText.trim();
 
 		smartTables.register("component", "Component Overview", {
 			"author": [
@@ -22,8 +22,7 @@ var PageHeader;
 			"classes": "Classes initialized",
 			"desc": "Short description"
 		}, function(table) {
-			var header = PageInner.querySelector("h1");
-			header.innerHTML = "<i class=\"fa fa-cube\"></i> Component: "+ header.innerHTML;
+			PageHeader.innerHTML = "<i class=\"fa fa-cube\"></i> Component: "+ Title;
 			CGHAPI.fetchContributors("lua/expadv/components/"+ Title +".lua")
 		});
 
@@ -33,7 +32,7 @@ var PageHeader;
 			"short": "Short name",
 			"lua-default": "Default value (as seen in Lua)"
 		}, function(table) {
-			PageHeader.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ PageHeader.innerHTML;
+			PageHeader.innerHTML = "<i class=\"fa fa-tag\"></i> Class: "+ Title;
 		});
 
 		smartTables.retrieve(PageInner);
